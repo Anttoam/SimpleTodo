4062064a6ad21cb37076172068f5233f2bc6861c
PenginAction <124853026+PenginAction@users.noreply.github.com>
2024-05-12 04:46:18 +0900

    add: implementation for connection db

---
+++ pkg/turso/db_conn.go
@@ -0,0 +1,22 @@
+package turso
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/Anttoam/golang-htmx-todos/config"
+
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
+)
+
+func NewLibsqlDB(cfg *config.Config) (*sql.DB, error) {
+	url := fmt.Sprintf("libsql://%s.turso.io?authToken=%s", cfg.Turso.Name, cfg.Turso.Token)
+
+	db, err := sql.Open("libsql", url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open db %s", err)
+		os.Exit(1)
+	}
+	return db, nil
+}
