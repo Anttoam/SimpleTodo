b98fc93eb2e0bc3d14b53572b3fe8457a85bcd77
PenginAction <124853026+PenginAction@users.noreply.github.com>
2024-05-12 07:46:29 +0900

    add: "uodate" response and response

--- dto/todo.go
+++ dto/todo.go
@@ -20,6 +20,22 @@ type CreateTodoResponse struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
+type UpdateTodoRequest struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	UserID    int       `json:"user_id"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type UpdateTodoResponse struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	UserID    int       `json:"user_id"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
 type FindAllTodoResponse struct {
 	Todos []*domain.Todo `json:"todos"`
 }
