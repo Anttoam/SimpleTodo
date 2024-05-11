15da7b67ba6974f4eac2b3f1cdc236d5381a9117
PenginAction <124853026+PenginAction@users.noreply.github.com>
2024-05-12 06:34:28 +0900

    add: signin and login responses

--- dto/user.go
+++ dto/user.go
@@ -1,12 +1,25 @@
 package dto
 
+import "time"
+
 type SignUpRequest struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
+type SignUpResponse struct {
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	ID    int    `json:"id"`
+	Email string `json:"email"`
 }
