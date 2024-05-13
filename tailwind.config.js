dd7e3fda861a25d5da198fabf0c4eb41b342d595
PenginAction <124853026+PenginAction@users.noreply.github.com>
2024-05-14 01:05:13 +0900

    update: add a-h templ

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./views/**/*.{html,templ}"],
   theme: {
     extend: {},
   },
