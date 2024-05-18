f444a95421f2d6100020bea059122eb5a755dbc2
PenginAction <124853026+PenginAction@users.noreply.github.com>
2024-05-18 22:59:51 +0900

    remove create and update responses

--- dto/todo.go
+++ dto/todo.go
@@ -12,26 +12,10 @@ type CreateTodoRequest struct {
 	UserID      int    `json:"user_id"`
 }
 
-type CreateTodoResponse struct {
-	Title       string    `json:"title"`
-	UserID      int       `json:"user_id"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
-}
-
 type UpdateTodoRequest struct {
 	ID          int       `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
-	UpdatedAt   time.Time `json:"updated_at"`
-}
-
-type UpdateTodoResponse struct {
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	UserID      int       `json:"user_id"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
 
