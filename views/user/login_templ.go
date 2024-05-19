548e49b795223021645c2d0edebbb4a3e38eb381
PenginAction <124853026+PenginAction@users.noreply.github.com>
2024-05-19 13:35:30 +0900

    change name NavBar to AuthNavBar

--- views/user/login_templ.go
+++ views/user/login_templ.go
@@ -39,7 +39,7 @@ func Login() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = NavBar().Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = AuthNavBar().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
