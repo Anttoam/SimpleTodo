42af1b2839fb3ce08bbacec8800784474ab6e819
PenginAction <124853026+PenginAction@users.noreply.github.com>
2024-06-05 13:20:17 +0900

    add url

--- config/redis.go
+++ config/redis.go
@@ -5,11 +5,13 @@ import "os"
 type RedisConfig struct {
 	Host string
 	Port string
+	Url  string
 }
 
 func LoadRedisConfig() RedisConfig {
 	return RedisConfig{
 		Host: os.Getenv("REDIS_HOST"),
 		Port: os.Getenv("REDIS_PORT"),
+		Url:  os.Getenv("REDIS_URL"),
 	}
 }
